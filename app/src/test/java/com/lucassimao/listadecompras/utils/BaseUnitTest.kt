12a95e0aef91002b419dadbca34fae1b72b7b70b
Lucas <oliveira.lucas20@gmail.com>
2022-09-07 22:18:02 -0400

    Implements test in view model

--- app/src/test/java/com/lucassimao/listadecompras/utils/BaseUnitTest.kt
+++ app/src/test/java/com/lucassimao/listadecompras/utils/BaseUnitTest.kt
@@ -1,9 +1,14 @@
 package com.lucassimao.listadecompras.utils
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Rule
 
 open class BaseUnitTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @get:Rule
+    var coroutinesTestRule = MainCoroutineScopeRule()
 }
