4ceb7a7ad4038045d56ebf82750f3ec493394c38
Lucas <oliveira.lucas20@gmail.com>
2023-09-24 12:17:23 -0400

    Refactors the Insert Screen tests

--- app/src/androidTest/java/com/lucassimao/listadecompras/ui/InsertScreenTest.kt
+++ app/src/androidTest/java/com/lucassimao/listadecompras/ui/InsertScreenTest.kt
@@ -1,13 +1,13 @@
 package com.lucassimao.listadecompras.ui
 
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performTextClearance
 import androidx.compose.ui.test.performTextInput
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.compose.NavHost
@@ -28,6 +28,20 @@ class InsertScreenTest {
 
     private lateinit var navController: TestNavHostController
 
+    private val textSave = "Salvar"
+    private val textCancel = "Cancelar"
+    private val textProductName = "Arroz"
+    private val textProductAmount = "2"
+    private val textProductPrice = "700"
+
+    private val textSuccessMessage = "Item salvo com sucesso"
+
+    private val tagProductNameField = "ProductNameField"
+    private val tagProductAmountField = "ProductAmountField"
+    private val tagProductPriceField = "ProductPriceField"
+
+    private val tagFab = "Fab"
+
     @Before
     fun setupAppNavHost() {
         composeTestRule.setContent {
@@ -39,7 +53,7 @@ class InsertScreenTest {
                 startDestination = Routes.HOME
             ) {
                 composable(Routes.HOME) { HomeScreen(navController) }
-                composable(Routes.INSERT) { InsertScreen() }
+                composable(Routes.INSERT) { InsertScreen(navController) }
             }
         }
     }
@@ -47,37 +61,52 @@ class InsertScreenTest {
     @Test
     fun when_product_name_quantity_and_price_fields_are_empty_save_button_is_disable() {
         goToInsertScreen()
-        composeTestRule.onNode(hasText("Salvar")).assertIsNotEnabled()
+        composeTestRule.onNode(hasText(textSave)).assertIsNotEnabled()
     }
 
     @Test
     fun when_product_name_field_is_filled_in_and_quantity_and_price_fields_are_empty_save_button_is_disable() {
         goToInsertScreen()
-        composeTestRule.onNode(hasTestTag("ProductNameField")).performTextInput("Arroz")
-        composeTestRule.onNode(hasText("Salvar")).assertIsNotEnabled()
+        composeTestRule.onNode(hasTestTag(tagProductNameField)).performTextInput(textProductName)
+        composeTestRule.onNode(hasText(textSave)).assertIsNotEnabled()
     }
 
     @Test
     fun when_product_name_and_quantity_fields_are_filled_in_and_price_field_is_empty_save_button_is_disable() {
         goToInsertScreen()
-        composeTestRule.onNode(hasTestTag("ProductNameField")).performTextInput("Arroz")
-        composeTestRule.onNode(hasTestTag("ProductAmountField")).performTextClearance()
-        composeTestRule.onNode(hasTestTag("ProductAmountField")).performTextInput("2")
-        composeTestRule.onNode(hasText("Salvar")).assertIsNotEnabled()
+        composeTestRule.onNode(hasTestTag(tagProductNameField)).performTextInput(textProductName)
+        composeTestRule.onNode(hasTestTag(tagProductAmountField)).performTextInput(textProductAmount)
+        composeTestRule.onNode(hasText(textSave)).assertIsNotEnabled()
     }
 
     @Test
     fun when_product_name_quantity_and_price_fields_are_filled_in_save_button_is_activated() {
         goToInsertScreen()
-        composeTestRule.onNode(hasTestTag("ProductNameField")).performTextInput("Arroz")
-        composeTestRule.onNode(hasTestTag("ProductAmountField")).performTextClearance()
-        composeTestRule.onNode(hasTestTag("ProductAmountField")).performTextInput("2")
-        composeTestRule.onNode(hasTestTag("ProductPriceField")).performTextInput("700")
-        composeTestRule.onNode(hasText("Salvar")).assertIsEnabled()
+        composeTestRule.onNode(hasTestTag(tagProductNameField)).performTextInput(textProductName)
+        composeTestRule.onNode(hasTestTag(tagProductAmountField)).performTextInput(textProductAmount)
+        composeTestRule.onNode(hasTestTag(tagProductPriceField)).performTextInput(textProductPrice)
+        composeTestRule.onNode(hasText(textSave)).assertIsEnabled()
+    }
+
+    @Test
+    fun when_enter_product_data_and_click_on_the_save_button_a_success_message_is_displayed(){
+        goToInsertScreen()
+        composeTestRule.onNode(hasTestTag(tagProductNameField)).performTextInput(textProductName)
+        composeTestRule.onNode(hasTestTag(tagProductAmountField)).performTextInput(textProductAmount)
+        composeTestRule.onNode(hasTestTag(tagProductPriceField)).performTextInput(textProductPrice)
+        composeTestRule.onNode(hasText(textSave)).performClick()
+        composeTestRule.onNode(hasText(textSuccessMessage)).assertIsDisplayed()
+    }
+
+    @Test
+    fun when_click_on_the_cancel_button_return_to_the_home_screen(){
+        goToInsertScreen()
+        composeTestRule.onNode(hasText(textCancel)).performClick()
+        composeTestRule.onNode(hasTestTag(tagFab)).assertExists()
     }
 
     private fun goToInsertScreen() {
-        composeTestRule.onNode(hasTestTag("Fab")).performClick()
+        composeTestRule.onNode(hasTestTag(tagFab)).performClick()
     }
 
 }
