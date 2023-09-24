8b07d7b66527882062b89c02ff7266b1b372ff7f
Lucas <oliveira.lucas20@gmail.com>
2023-09-24 12:41:55 -0400

    Adds Scaffold. Snackbar to show success message. Return to home screen function. Function to save product data to the database. And small changes

--- app/src/main/java/com/lucassimao/listadecompras/ui/compose/BaseContent.kt
+++ app/src/main/java/com/lucassimao/listadecompras/ui/compose/BaseContent.kt
@@ -1,9 +1,9 @@
 package com.lucassimao.listadecompras.ui.compose
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
@@ -11,13 +11,18 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -25,24 +30,32 @@ import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.lucassimao.listadecompras.R
+import com.lucassimao.listadecompras.ui.PurchaseViewModel
 import com.lucassimao.listadecompras.utils.CurrencyAmountInputVisualTransformation
+import com.lucassimao.listadecompras.utils.removeLastTwoCharacters
+import kotlinx.coroutines.launch
+import org.koin.androidx.compose.koinViewModel
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BaseContent(modifier: Modifier = Modifier) {
+fun BaseContent(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+    viewModel: PurchaseViewModel = koinViewModel()
+) {
 
-    var productName by remember {
-        mutableStateOf("")
-    }
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
 
-    var productAmount by remember {
-        mutableStateOf("1")
-    }
+    var productName by remember { mutableStateOf("") }
 
-    var productPrice by remember {
-        mutableStateOf("")
-    }
+    var productAmount by remember { mutableStateOf("") }
+
+    var productPrice by remember { mutableStateOf("") }
 
     val isProductNameEmpty = productName.isBlank()
     val isProductAmountEmpty = productAmount.isBlank()
@@ -52,92 +65,111 @@ fun BaseContent(modifier: Modifier = Modifier) {
 
     val fullWidthModifier = modifier.fillMaxWidth()
 
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp)
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        containerColor = Color.White
     ) {
-
-        OutlinedTextField(
-            value = productName,
-            modifier = fullWidthModifier.testTag("ProductNameField"),
-            onValueChange = {
-                productName = it
-            },
-            label = {
-                Text(text = stringResource(R.string.product_name))
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                capitalization = KeyboardCapitalization.Words,
-                imeAction = ImeAction.Next
-            ),
-            singleLine = true
-        )
-
-        OutlinedTextField(
-            value = productAmount,
-            modifier = fullWidthModifier.testTag("ProductAmountField"),
-            onValueChange = {
-                if (it.length <= 3) {
-                    productAmount = it
-                }
-            },
-            label = {
-                Text(text = stringResource(R.string.product_amount))
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next
-            )
-        )
-
-        OutlinedTextField(
-            value = productPrice,
-            modifier = fullWidthModifier.testTag("ProductPriceField"),
-            onValueChange = {
-                productPrice = if (it.startsWith("0")) {
-                    ""
-                } else {
-                    it
-                }
-            },
-            label = {
-                Text(text = stringResource(R.string.product_price))
-            },
-            visualTransformation = CurrencyAmountInputVisualTransformation(),
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Decimal,
-                imeAction = ImeAction.Done
-            ),
-            singleLine = true,
-        )
-
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            modifier = modifier.fillMaxWidth()
+        Column(
+            modifier = modifier
+                .padding(16.dp)
         ) {
 
-            val weightButton = modifier.weight(1f)
+            OutlinedTextField(
+                value = productName,
+                modifier = fullWidthModifier.testTag("ProductNameField"),
+                onValueChange = {
+                    productName = it
+                },
+                label = {
+                    Text(text = stringResource(R.string.product_name))
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    capitalization = KeyboardCapitalization.Words,
+                    imeAction = ImeAction.Next
+                ),
+                singleLine = true
+            )
 
-            OutlinedButton(
-                onClick = { /*TODO*/ },
-                modifier = weightButton
-                    .padding(8.dp),
+            OutlinedTextField(
+                value = productAmount,
+                modifier = fullWidthModifier.testTag("ProductAmountField"),
+                onValueChange = {
+                    if (it.length <= 3) {
+                        productAmount = it
+                    }
+                },
+                label = {
+                    Text(text = stringResource(R.string.product_amount))
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                )
+            )
+
+            OutlinedTextField(
+                value = productPrice,
+                modifier = fullWidthModifier.testTag("ProductPriceField"),
+                onValueChange = {
+                    productPrice = if (it.startsWith("0")) {
+                        ""
+                    } else {
+                        it
+                    }
+                },
+                label = {
+                    Text(text = stringResource(R.string.product_price))
+                },
+                visualTransformation = CurrencyAmountInputVisualTransformation(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Decimal,
+                    imeAction = ImeAction.Done
+                ),
+                singleLine = true,
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = modifier.fillMaxWidth()
             ) {
-                Text(text = stringResource(R.string.cancel))
+
+                OutlinedButton(
+                    onClick = {
+                        navController.popBackStack()
+                    },
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(8.dp),
+                ) {
+                    Text(text = stringResource(R.string.cancel))
+                }
+
+                Button(
+                    onClick = {
+                        val result = productPrice.removeLastTwoCharacters()
+                        viewModel.insert(productName, productAmount.toInt(), result)
+                        productName = ""
+                        productAmount = ""
+                        productPrice = ""
+                        scope.launch {
+                            snackbarHostState.showSnackbar("Item salvo com sucesso")
+                        }
+                    },
+                    modifier = modifier
+                        .weight(1f)
+                        .padding(8.dp),
+                    enabled = !isAnyFieldEmpty
+                ) {
+                    Text(text = stringResource(R.string.save))
+                }
             }
 
-            Button(
-                onClick = { /*TODO*/ },
-                modifier = weightButton.padding(8.dp),
-                enabled = !isAnyFieldEmpty
-            ) {
-                Text(text = stringResource(R.string.save))
-            }
         }
-
     }
+
 
 }
 
@@ -145,5 +177,6 @@ fun BaseContent(modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun Preview() {
-    BaseContent()
+    val navController: NavHostController = rememberNavController()
+    BaseContent(navController)
 }
