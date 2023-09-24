8a4f04ce97d7ae19f48a6437c895d8931dffc792
Lucas <oliveira.lucas20@gmail.com>
2023-09-24 14:21:59 -0400

    Comments code

--- app/src/androidTest/java/com/lucassimao/listadecompras/utils/BaseUITest.kt
+++ app/src/androidTest/java/com/lucassimao/listadecompras/utils/BaseUITest.kt
@@ -3,8 +3,8 @@ package com.lucassimao.listadecompras.utils
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
-open class BaseUITest {
-//    @get:Rule
-//    val activityRule = ActivityScenarioRule(MainActivity::class.java)
-}
+//@RunWith(AndroidJUnit4::class)
+//open class BaseUITest {
+////    @get:Rule
+////    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+//}
