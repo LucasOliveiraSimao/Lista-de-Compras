de781763d24dbb01eb36051c52d8229a2393d702
Lucas <oliveira.lucas20@gmail.com>
2023-09-24 20:58:30 -0400

    Moves test configuration codes to a file

--- app/src/androidTest/java/com/lucassimao/listadecompras/utils/BaseUITest.kt
+++ app/src/androidTest/java/com/lucassimao/listadecompras/utils/BaseUITest.kt
@@ -1,10 +1,35 @@
 package com.lucassimao.listadecompras.utils
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.runner.RunWith
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.testing.TestNavHostController
+import com.lucassimao.listadecompras.ui.compose.home.HomeScreen
+import com.lucassimao.listadecompras.ui.compose.insert.InsertScreen
+import org.junit.Before
+import org.junit.Rule
 
-//@RunWith(AndroidJUnit4::class)
-//open class BaseUITest {
-////    @get:Rule
-////    val activityRule = ActivityScenarioRule(MainActivity::class.java)
-//}
+open class BaseUITest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupAppNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+
+            NavHost(
+                navController = navController,
+                startDestination = Routes.HOME
+            ) {
+                composable(Routes.HOME) { HomeScreen(navController) }
+                composable(Routes.INSERT) { InsertScreen(navController) }
+            }
+        }
+    }
+}
