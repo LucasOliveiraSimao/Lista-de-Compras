515959d3d96143a2fdd2dc7feab7d55e116761b6
Lucas <oliveira.lucas20@gmail.com>
2023-10-03 18:44:54 -0400

    Implements function to add the total of products

--- app/src/main/java/com/lucassimao/listadecompras/ui/compose/home/HomeContent.kt
+++ app/src/main/java/com/lucassimao/listadecompras/ui/compose/home/HomeContent.kt
@@ -8,17 +8,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.lucassimao.listadecompras.R
 import com.lucassimao.listadecompras.ui.PurchaseViewModel
+import com.lucassimao.listadecompras.utils.calculateTotalPurchase
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -26,25 +30,50 @@ fun HomeContent(
     modifier: Modifier = Modifier,
     viewModel: PurchaseViewModel = koinViewModel()
 ) {
+
+    val purchases = viewModel.getAllPurchase.collectAsState(initial = emptyList())
+
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(top = 16.dp)
+            .padding(8.dp)
     ) {
         Row(
-            modifier = modifier.fillMaxWidth(),
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(8.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text(text = "Total: R$ 100,00", modifier = modifier.align(Alignment.CenterVertically))
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = stringResource(id = R.string.delete_everything))
+            Text(
+                modifier = modifier
+                    .align(Alignment.CenterVertically)
+                    .weight(1f)
+                    .testTag("Total"),
+                text = "Total: R$ ${calculateTotalPurchase(purchases.value)}",
+                color = Color.White,
+                fontSize = 20.sp
+            )
+            OutlinedButton(
+                modifier = modifier
+                    .weight(1f)
+                    .testTag("Delete"),
+                onClick = {
+                    viewModel.deleteAllPurchase()
+                }) {
+                Text(
+                    text = stringResource(id = R.string.delete_everything),
+                    color = Color.White,
+                    fontSize = 16.sp
+                )
             }
         }
 
-        val productList = viewModel.getAllPurchase.collectAsState(initial = emptyList())
-
-        LazyColumn(modifier = Modifier.padding(8.dp)) {
-            items(productList.value) {
+        LazyColumn(
+            modifier = modifier
+                .padding(8.dp)
+                .testTag("List")
+        ) {
+            items(purchases.value) {
                 ItemList(it)
             }
         }
