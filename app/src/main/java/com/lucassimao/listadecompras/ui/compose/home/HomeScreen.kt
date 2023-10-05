59c8d6d396745dc9482dd6d3c31fe9c7abfec688
Lucas <oliveira.lucas20@gmail.com>
2023-10-05 13:58:41 -0400

    Makes small changes

--- app/src/main/java/com/lucassimao/listadecompras/ui/compose/home/HomeScreen.kt
+++ app/src/main/java/com/lucassimao/listadecompras/ui/compose/home/HomeScreen.kt
@@ -7,26 +7,50 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.lucassimao.listadecompras.R
+import com.lucassimao.listadecompras.ui.PurchaseViewModel
 import com.lucassimao.listadecompras.utils.Routes.INSERT
+import org.koin.androidx.compose.koinViewModel
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun HomeScreen(navController: NavHostController = rememberNavController()) {
+fun HomeScreen(
+    navController: NavHostController = rememberNavController(),
+    viewModel: PurchaseViewModel = koinViewModel(),
+    modifier: Modifier = Modifier,
+) {
+    val purchases = viewModel.getAllPurchase.collectAsState(initial = emptyList())
+
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
         content = {
-            HomeContent()
+            HomeContent(modifier, purchases, viewModel, scope, snackbarHostState)
         },
         floatingActionButton = {
-            FloatingActionButton(onClick = {
-                navController.navigate(INSERT)
-            }, modifier = Modifier.testTag("Fab")) {
+            FloatingActionButton(
+                onClick = {
+                    navController.navigate(INSERT)
+                }, modifier = Modifier
+                    .testTag(stringResource(R.string.tag_test_button_add))
+            ) {
                 Icon(Icons.Filled.Add, contentDescription = null)
             }
         }
