41d49e85ccfa22a8792c19558b3fe41e574260e3
Lucas <oliveira.lucas20@gmail.com>
2023-10-23 21:59:11 -0400

    Refactors and rename tests

---
+++ app/src/androidTest/java/com/lucassimao/listadecompras/ui/PurchaseListDisplayFunction.kt
@@ -0,0 +1,63 @@
+package com.lucassimao.listadecompras.ui
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.performClick
+import com.lucassimao.listadecompras.utils.BaseUITest
+import org.junit.Test
+
+class PurchaseListDisplayFunction : BaseUITest() {
+
+    private val total = "Total: R$ 42,00"
+    private val buttonDelete = "button_delete"
+
+    @Test
+    fun should_add_new_purchase_and_display_in_the_list() {
+        goToInsertScreen()
+        addProduct()
+        saveProduct()
+
+        composeTestRule.onNode(hasText(rice)).assertIsDisplayed()
+        composeTestRule.onNode(hasText(quantity)).assertIsDisplayed()
+        composeTestRule.onNode(hasText(price14)).assertIsDisplayed()
+    }
+
+    @Test
+    fun should_delete_all_purchase_list() {
+        goToInsertScreen()
+        addProduct()
+        saveProduct()
+
+        goToInsertScreen()
+        addProduct()
+        saveProduct()
+
+        goToInsertScreen()
+        addProduct()
+        saveProduct()
+
+        composeTestRule.onNode(hasTestTag(buttonDelete)).performClick()
+        composeTestRule.onNode(hasTestTag(purchaseList)).assertIsNotDisplayed()
+
+    }
+
+    @Test
+    fun should_display_total_purchase() {
+        goToInsertScreen()
+        addProduct()
+        saveProduct()
+
+        goToInsertScreen()
+        addProduct()
+        saveProduct()
+
+        goToInsertScreen()
+        addProduct()
+        saveProduct()
+
+        composeTestRule.onNode(hasText(total)).assertExists()
+    }
+
+}
